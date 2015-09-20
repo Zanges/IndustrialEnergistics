67651682d38bc8de53963eed0b0d0b296b4f1fc1
zanges <zanges93@gmail.com>
2015-09-20 02:46:54 +0200

    Preping Main Class

--- src/main/java/net/zanges/industrialenergistics/IndustrialEnergistics.java
+++ src/main/java/net/zanges/industrialenergistics/IndustrialEnergistics.java
@@ -1,18 +1,38 @@
 package net.zanges.industrialenergistics;
 
-import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.zanges.industrialenergistics.proxy.IProxy;
 import net.zanges.industrialenergistics.reference.Reference;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class IndustrialEnergistics
 {
+    @Mod.Instance(Reference.MOD_ID)
+    public static IndustrialEnergistics instance;
+
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
+    public static IProxy proxy;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
     }
 }
---
+++ src/main/java/net/zanges/industrialenergistics/client/gui/ModGuiConfig.java
@@ -0,0 +1,22 @@
+package net.zanges.industrialenergistics.client.gui;
+
+import cpw.mods.fml.client.config.GuiConfig;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.common.config.ConfigElement;
+import net.minecraftforge.common.config.Configuration;
+import net.zanges.industrialenergistics.handler.ConfigurationHandler;
+import net.zanges.industrialenergistics.reference.Reference;
+
+public class ModGuiConfig extends GuiConfig
+{
+
+    public ModGuiConfig(GuiScreen guiScreen)
+    {
+        super(guiScreen,
+                new ConfigElement(ConfigurationHandler.configuration.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements(),
+                Reference.MOD_ID,
+                false,
+                false,
+                GuiConfig.getAbridgedConfigPath(ConfigurationHandler.configuration.toString()));
+    }
+}
